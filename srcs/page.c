68ff143faea9c21c19d5751800661a510adc1c9a
jrimpila <jrimpila@student.hive.fi>
2025-04-02 18:08:06 +0300

    made return value updating work, changed nl to `newline'

--- srcs/page.c
+++ srcs/page.c
@@ -6,7 +6,7 @@
 /*   By: jrimpila <jrimpila@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 11:20:15 by jrimpila          #+#    #+#             */
-/*   Updated: 2025/04/02 17:57:04 by jrimpila         ###   ########.fr       */
+/*   Updated: 2025/04/02 18:06:18 by jrimpila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ t_sent	*conv_linked_to_sentence(int i, int k, t_node *node, t_sent *sentence)
 		{
 			if (node->next->type == REDIRECT || node->next->type == PIPE
 				|| get_data()->tokens.last == node)
-				return (syntax_error("nl"));
+				return (syntax_error("`newline'"));
 		}
 		else if (is_file(node->type))
 			add_redirection(node, sentence, k++);
--- srcs/redir.c
+++ srcs/redir.c
@@ -6,7 +6,7 @@
 /*   By: jrimpila <jrimpila@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 15:39:47 by jrimpila          #+#    #+#             */
-/*   Updated: 2025/04/02 16:31:56 by jrimpila         ###   ########.fr       */
+/*   Updated: 2025/04/02 18:05:12 by jrimpila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,7 @@ void	add_redirection(t_node *node, t_sent *sentence, int i)
 	{
 		deallocate(get_data());
 		error_printf("", "syntax error near unexpected token `newline'");
-		add_envvar(get_data()->env, "?", "2");
+		store_return_value(2, true);
 		return ;
-		//sentence->redirs[i].path = ft_strdup("\0");
 	}
 }
