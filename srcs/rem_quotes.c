ad1b4307746d2645b581b33ea917ce52737b5ccc
jtuomi <jtuomi@student.hive.fi>
2025-04-05 16:21:30 +0300

    Revert "Update rem_quotes.c"
    This reverts commit 8a41e02eb38b928bd72e1fdc27b2484f5eecd3bf.

--- srcs/rem_quotes.c
+++ srcs/rem_quotes.c
@@ -12,9 +12,8 @@
 
 #include "../include/minishell.h"
 
-/*
-**  checks whether the next character is a delimiter
-*/
+//jos seuraava ei ole alnum eika _ eika "" eika '' eika ?
+
 int	next_is_delim(char *str, int i, t_char *dst, int k)
 {
 	if (k && dst[k - 1].added)
@@ -25,12 +24,13 @@ int	next_is_delim(char *str, int i, t_char *dst, int k)
 	while (str[i] != 0 && str[i] != ' ')
 		if (str[i] == '\'' && str[i] == '\'')
 			i += 2;
-	else if (str[i] == '\"' && str[i] == '\"')
-		i += 2;
-	else
-		return (0);
+		else if (str[i] == '\"' && str[i] == '\"')
+			i += 2;
+		else
+			return (0);
 	return (1);
 }
+//need a specific test for pipe
 
 int	mark_redir(char *src, int *i, t_char *dst, int *k)
 {
@@ -47,8 +47,8 @@ int	mark_redir(char *src, int *i, t_char *dst, int *k)
 		(*k)++;
 		return (1);
 	}
-	else if (src[(*i) + 1] != src[*i] && (src[*i] == '>' || src[*i] == '<'
-			|| src[*i] == '|'))
+	else if (src[(*i) + 1] != src[*i] && (src[*i] == '>' || \
+	src[*i] == '<' || src[*i] == '|' ))
 	{
 		dst[*k].c = ' ';
 		(*k)++;
