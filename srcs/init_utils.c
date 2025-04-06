690a508980ee9a94986bbfac8843b028c045dfbd
jrimpila <jrimpila@student.hive.fi>
2025-04-06 18:44:35 +0300

    Passing env pointer to execve should work now with the new get_own_env(), buffer overflow should be guarded with exit.

--- srcs/init_utils.c
+++ srcs/init_utils.c
@@ -6,7 +6,7 @@
 /*   By: jrimpila <jrimpila@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 11:24:54 by jrimpila          #+#    #+#             */
-/*   Updated: 2025/03/01 20:58:55 by jrimpila         ###   ########.fr       */
+/*   Updated: 2025/04/06 17:32:25 by jrimpila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,28 @@ int	copy_env_to_tchar(t_char *dst, int i, const char *env)
 		env_i++;
 	}
 	return (i);
+}
+
+char **get_own_env(void)
+{
+    static char *cpy[ENV_SIZE + 1];
+    int i;
+	int	k;
+	t_data *data;
+
+	data = get_data();
+	i = 0;
+	k = 0;
+	ft_memset(cpy, 0, sizeof(cpy));
+    while (k < ENV_SIZE)
+    {
+        if (data->env[k][0] != '\0')
+        {
+            cpy[i] = data->env[k];
+            i++;
+        }
+        k++;
+    }
+    cpy[i] = NULL;
+    return cpy;
 }
